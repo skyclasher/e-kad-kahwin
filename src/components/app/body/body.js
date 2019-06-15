eabea70d7351c4601d60d37b7e158f1c6afbba93
Ahmad Syafiq <asyafiq99@gmail.com>
2019-06-15 16:51:43 +0800

    commit

--- src/components/app/body/body.js
+++ src/components/app/body/body.js
@@ -1,22 +1,14 @@
 import React from 'react';
+import Section1 from './section-1/section-1';
 import './body.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'	
 
 export default function Body() {
   return (
-	  <div>
-	<div className="container-fluid">
-		Section 1
-	</div>
-	<div className="container-fluid">
-		Section 2
-	</div>
-	<div className="container-fluid">
-		Section 3
-	</div>
-	<div className="container-fluid">
-		Section 4
-	</div>
+	<div>
+		<Section1 />
+		<Section1 />
+		<Section1 />
+		<Section1 />
 	</div>
   );
 }
