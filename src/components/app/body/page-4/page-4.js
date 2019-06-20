3153148a968110fcbe603b45c7854702a84dfb55
Ahmad Syafiq <asyafiq99@gmail.com>
2019-06-20 08:49:39 +0800

    adjust height page 4

--- src/components/app/body/page-4/page-4.js
+++ src/components/app/body/page-4/page-4.js
@@ -8,13 +8,14 @@ import SectionBottom from './section-bottom/section-bottom';
 let page4 = () => {
   return (
 	<div className="page4 padding-top-40 video-container">
-		
+		<div className="content-center-page4">
 		<video playsInline loop autoPlay muted className="video-bg">
     		<source src="/assets/gif/Background_Bokeh_03.mov" type="video/mp4" />
   		</video>
 
 		<div className="center-content-page4">
 			<SectionTop />
+		</div>
 		</div>
   	</div>
   );
